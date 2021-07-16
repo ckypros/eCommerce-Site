fbaaa034440983f9f7d5f0592fd20596c2df1fa9
ckypros <ckypros@gmail.com>
2021-07-15 17:28:34 -0700

    ratings complete

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,36 @@
+import PropTypes from 'prop-types';
+
+const Rating = ({ value, numRatings, color }) => {
+    const getStar = (starNum) => {
+        const star =
+            value >= starNum
+                ? 'fas fa-star'
+                : value > starNum - 1
+                ? 'fas fa-star-half-alt'
+                : 'far fa-star';
+        return <i style={{ color }} key={starNum} className={star} />;
+    };
+
+    return (
+        <div className='rating'>
+            {[1, 2, 3, 4, 5].map((val) => {
+                return getStar(val);
+            })}
+            <span>
+                {numRatings} rating{numRatings !== 1 && 's'}
+            </span>
+        </div>
+    );
+};
+
+Rating.defaultProps = {
+    color: '#f8e825',
+};
+
+Rating.propTypes = {
+    value: PropTypes.number.isRequired,
+    numRatings: PropTypes.number.isRequired,
+    color: PropTypes.string,
+};
+
+export default Rating;
