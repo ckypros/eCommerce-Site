2199a39f4c60055ef256e8bb49d3431be553f039
ckypros <ckypros@gmail.com>
2021-07-23 18:04:00 -0700

    Added user authorization to profile using jsonwebtoken

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import dotenv from 'dotenv';
 import colors from 'colors';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -11,11 +13,15 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
+// mount the routes
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 // Error Handler Middleware
 app.use(notFound);
@@ -25,8 +31,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
     5000,
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-            .bold
-    )
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
